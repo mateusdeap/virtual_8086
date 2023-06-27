a31fa89f4aa7140fefd0a2c02488c36fdc617c2d
Mateus Pereira <mateusdeap@gmail.com>
2023-06-27 11:56:12 -0300

    Change everything to use binary notation

--- lib/virtual8086.ex
+++ lib/virtual8086.ex
@@ -41,28 +41,28 @@ defmodule Virtual8086 do
     |> do_disassemble()
   end
 
-  def decode(<<34::6, d_field::1, w_field::1>>, <<mode::2, register::3, rm::3, rest::binary>>) do
+  def decode(<<0b100010::6, d_field::1, w_field::1>>, <<mode::2, register::3, rm::3, rest::binary>>) do
     %{assembly: "mov #{operands(d_field, w_field, mode, register, rm)}\n", remaining_binary: rest}
   end
 
-  def decode(<<11::4, 0::1, register::3>>, <<data::8, rest::binary>>) do
+  def decode(<<0b1011::4, 0b0::1, register::3>>, <<data::8, rest::binary>>) do
     %{assembly: "mov #{operands(0, register, data)}\n", remaining_binary: rest}
   end
 
-  def decode(<<11::4, 1::1, register::3>>, <<data_lo::8, data_hi::8, rest::binary>>) do
+  def decode(<<0b1011::4, 0b1::1, register::3>>, <<data_lo::8, data_hi::8, rest::binary>>) do
     <<data::16>> = <<data_hi::8, data_lo::8>>
-    %{assembly: "mov #{operands(1, register, data)}\n", remaining_binary: rest}
+    %{assembly: "mov #{operands(0b1, register, data)}\n", remaining_binary: rest}
   end
 
   def append_to_assembly(instruction, assembly) do
     %{assembly: assembly ++ [instruction[:assembly]], remaining_binary: instruction[:remaining_binary]}
   end
 
-  def operands(0, w_field, mode, register, rm) do
+  def operands(0b0, w_field, mode, register, rm) do
     "#{source_operand(w_field, mode, rm)}, #{destination_operand(w_field, register)}"
   end
 
-  def operands(1, w_field, mode, register, rm) do
+  def operands(0b1, w_field, mode, register, rm) do
     "#{destination_operand(w_field, register)}, #{source_operand(w_field, mode, rm)}"
   end
 
@@ -70,19 +70,19 @@ defmodule Virtual8086 do
     "#{destination_operand(w_field, register)}, #{data}"
   end
 
-  def destination_operand(0, register) do
+  def destination_operand(0b0, register) do
     @byte_registers[binary_digits(register)]
   end
 
-  def destination_operand(1, register) do
+  def destination_operand(0b1, register) do
     @word_registers[binary_digits(register)]
   end
 
-  def source_operand(0, 3, rm) do
+  def source_operand(0b0, 0b011, rm) do
     @byte_registers[binary_digits(rm)]
   end
 
-  def source_operand(1, 3, rm) do
+  def source_operand(0b1, 0b011, rm) do
     @word_registers[binary_digits(rm)]
   end
 
